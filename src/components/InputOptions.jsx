aebf05dfc2691ba7f8176e8f46d1f37959156fb1
Supakorn <supakorn.i@pm.me>
2022-05-19 19:37:24 +0700

    initial

--- src/components/InputOptions.jsx
+++ src/components/InputOptions.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import styled from "styled-components";
 
-const InputOption = syled.div``;
+const InputOption = styled.div``;
 
 const InputOptions = ({ Icon, title, color }) => {
   return (
