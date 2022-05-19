c871e3bab390f7f592c305150ec1aca6731b277b
Supakorn <supakorn.i@pm.me>
2022-05-19 18:43:23 +0700

    initial

--- src/layouts/Feed.jsx
+++ src/layouts/Feed.jsx
@@ -1,8 +1,20 @@
 import React from "react";
 import styled from "styled-components";
+import CreateIcon from "@mui/icons-material/Create";
 
+const FeedContainer = styled.div``;
+
+const InputContainer = styled.div``;
 const Feed = () => {
-  return <div>Feed</div>;
+  return (
+    <FeedContainer>
+      <InputContainer>
+        <div className="input">
+          <CreateIcon />
+        </div>
+      </InputContainer>
+    </FeedContainer>
+  );
 };
 
 export default Feed;
