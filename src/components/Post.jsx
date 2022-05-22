df12fd3b7cc113dd29ce7980b40e444b7588f3b7
Supakorn <supakorn.i@pm.me>
2022-05-22 13:36:32 +0700

    initial

--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -2,10 +2,17 @@ import React from "react";
 import styled from "styled-components";
 import Avatar from "@mui/material/Avatar";
 
-const PostContainer = styled.div``;
+const PostContainer = styled.div`
+  background-color: white;
+  padding: 15px;
+  margin-bottom: 10px;
+  border-radius: 10px;
+`;
+
 const PostHeader = styled.div``;
 const PostBody = styled.div``;
 const PostInfo = styled.div``;
+
 const Post = ({ name, description, message, photoUrl }) => {
   return (
     <PostContainer>
