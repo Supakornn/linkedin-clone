f288bdb4a852ec0b508042651c6af28ecb51dfa1
Supakorn <supakorn0728@gmail.com>
2022-05-28 21:56:19 +0700

    initial

--- src/layouts/Login.jsx
+++ src/layouts/Login.jsx
@@ -3,129 +3,128 @@ import styled from "styled-components";
 import { auth } from "../firebase";
 // styled-components
 const LoginContainer = styled.div`
-  display: grid;
-  place-items: center;
-  margin-left: auto;
-  margin-right: auto;
-  padding-top: 100px;
-  padding-bottom: 100px;
+    display: grid;
+    place-items: center;
+    margin-left: auto;
+    margin-right: auto;
+    padding-top: 100px;
+    padding-bottom: 100px;
 
-  > img {
-    object-fit: contain;
-    height: 150px;
-    margin-bottom: 20px;
-    margin-top: 20px;
-  }
-
-  > form {
-    display: flex;
-    flex-direction: column;
-    > input {
-      width: 350px;
-      height: 50px;
-      font-size: 20px;
-      padding-left: 10px;
-      margin-bottom: 10px;
-      border-radius: 5px;
+    > img {
+        object-fit: contain;
+        height: 150px;
+        margin-bottom: 20px;
+        margin-top: 20px;
     }
-    > button {
-      width: 365px;
-      height: 50px;
-      font-size: large;
-      color: #fff;
-      background-color: #0074b1;
-      border-radius: 5px;
+
+    > form {
+        display: flex;
+        flex-direction: column;
+        > input {
+            width: 350px;
+            height: 50px;
+            font-size: 20px;
+            padding-left: 10px;
+            margin-bottom: 10px;
+            border-radius: 5px;
+        }
+        > button {
+            width: 365px;
+            height: 50px;
+            font-size: large;
+            color: #fff;
+            background-color: #0074b1;
+            border-radius: 5px;
+        }
     }
-  }
 
-  .register {
-    color: #0177b7;
-    cursor: pointer;
-  }
+    .register {
+        color: #0177b7;
+        cursor: pointer;
+    }
 
-  > p {
-    margin-top: 20px;
-  }
+    > p {
+        margin-top: 20px;
+    }
 `;
 const Login = () => {
-  const [email, setEmail] = useState("");
-  const [password, setPassword] = useState("");
-  const [name, setName] = useState("");
-  const [pic, setPic] = useState("");
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+    const [name, setName] = useState("");
+    const [pic, setPic] = useState("");
 
-  const login = (e) => {
-    e.preventDefault();
-  };
+    const login = (e) => {
+        e.preventDefault();
+    };
 
-  const register = () => {
-    if (!name) {
-      return alert("Please enter your name");
-    }
-    auth
-      .createUserWithEmailAndPassword(email, password)
-      .then((userAuth) => {
-        userAuth.user
-          .updateProfile({
-            displayName: name,
-            photoURL: pic
-          })
-          .then(() => {
-            dispatchEvent(
-              login({
-                email: userAuth.user.email,
-                uid: userAuth.user.uid,
-                displayname: name,
-                photoURL: pic
-              })
-            );
-          });
-      })
-      .catch((error) => alert(error.message));
-  };
+    const register = () => {
+        if (!name) {
+            return alert("Please enter your name");
+        }
+        auth.createUserWithEmailAndPassword(email, password)
+            .then((userAuth) => {
+                userAuth.user
+                    .updateProfile({
+                        displayName: name,
+                        photoURL: pic
+                    })
+                    .then(() => {
+                        dispatchEvent(
+                            login({
+                                email: userAuth.user.email,
+                                uid: userAuth.user.uid,
+                                displayname: name,
+                                photoURL: pic
+                            })
+                        );
+                    });
+            })
+            .catch((error) => alert(error.message));
+    };
 
-  return (
-    <LoginContainer>
-      <img
-        src="https://logos-world.net/wp-content/uploads/2020/04/Linkedin-Logo-2011-2019.png"
-        alt=""
-      />
-      <form>
-        <input
-          type="text"
-          placeholder="Full name (required if registering)"
-          value={name}
-          onChange={(e) => setName(e.target.value)}
-        />
-        <input
-          type="text"
-          placeholder="Profile pic URL (optional)"
-          value={pic}
-          onChange={(e) => setPic(e.target.value)}
-        />
-        <input
-          type="email"
-          placeholder="Email"
-          value={email}
-          onChange={(e) => setEmail(e.target.value)}
-        />
-        <input
-          type="password"
-          placeholder="Password"
-          value={password}
-          onChange={(e) => setPassword(e.target.value)}
-        />
-        <button type="submit" onClick={login}>
-          Sign In
-        </button>
-      </form>
-      <p>
-        Not a member? {""}
-        <span className="register" onClick={register}>
-          Register Now
-        </span>
-      </p>
-    </LoginContainer>
-  );
+    return (
+        <LoginContainer>
+            <img
+                src="https://logos-world.net/wp-content/uploads/2020/04/Linkedin-Logo-2011-2019.png"
+                alt=""
+            />
+            <form>
+                <input
+                    type="text"
+                    placeholder="Full name (required if registering)"
+                    value={name}
+                    onChange={(e) => setName(e.target.value)}
+                />
+                <input
+                    type="text"
+                    placeholder="Profile pic URL (optional)"
+                    value={pic}
+                    onChange={(e) => setPic(e.target.value)}
+                />
+                <input
+                    type="email"
+                    placeholder="Email"
+                    value={email}
+                    onChange={(e) => setEmail(e.target.value)}
+                />
+                <input
+                    type="password"
+                    placeholder="Password"
+                    value={password}
+                    onChange={(e) => setPassword(e.target.value)}
+                />
+                <button type="submit" onClick={login}>
+                    Sign In
+                </button>
+            </form>
+            <p>
+                Not a member? {""}
+                <span className="register" onClick={register}>
+                    Register Now
+                </span>
+            </p>
+        </LoginContainer>
+    );
 };
 
 export default Login;
