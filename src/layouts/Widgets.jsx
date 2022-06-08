d89ed3d55c3ea292a71a1f630c9076f253a57a85
Supakorn <supakorn0728@gmail.com>
2022-06-08 22:09:41 +0700

    initial

--- src/layouts/Widgets.jsx
+++ src/layouts/Widgets.jsx
@@ -1,14 +1,29 @@
 import React from "react";
 import styled from "styled-components";
+import InfoIcon from "@mui/icons-material/Info";
 
-const WidgetsContainer = styled.div``;
+const WidgetsContainer = styled.div`
+  position: sticky;
+  top: 80px;
+  flex: 0.2;
+  background-color: white;
+  border-radius: 10px;
+  border: 1px solid lightgray;
+  height: fit-content;
+  padding-bottom: 10px;
+`;
 
-const WidgetsHeader = styled.div``;
+const WidgetsHeader = styled.div`
+  display: flex;
+`;
 
 const Widgets = () => {
   return (
     <WidgetsContainer>
-      <WidgetsHeader></WidgetsHeader>
+      <WidgetsHeader>
+        <h2>LinkedIn News</h2>
+        <InfoIcon />
+      </WidgetsHeader>
     </WidgetsContainer>
   );
 };
