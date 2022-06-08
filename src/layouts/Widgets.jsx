f36e3b81e9979b9eeed9580c4c4ba167f7c7cf79
Supakorn <supakorn0728@gmail.com>
2022-06-08 22:36:39 +0700

    initial

--- src/layouts/Widgets.jsx
+++ src/layouts/Widgets.jsx
@@ -15,6 +15,14 @@ const WidgetsContainer = styled.div`
 
 const WidgetsHeader = styled.div`
   display: flex;
+  align-items: center;
+  justify-content: space-between;
+  padding: 10px;
+
+  > h2 {
+    font-size: 16px;
+    font-weight: 400;
+  }
 `;
 
 const Widgets = () => {
